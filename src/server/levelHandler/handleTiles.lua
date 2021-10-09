892c07a83fd68f5cda946e4d53665134db01f1cc
Cigatronix <33050240+Cigatronix@users.noreply.github.com>
2021-10-09 16:54:47 -0600

    Handle connection for touched tiles

--- src/server/levelHandler/handleTiles.lua
+++ src/server/levelHandler/handleTiles.lua
@@ -1,4 +1,5 @@
 --- ( Services ) ---
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local CollectionService = game:GetService("CollectionService")
 
@@ -10,9 +11,86 @@ local tiles = ReplicatedStorage:WaitForChild("Tiles")
 
 --- ( Private Variables ) ---
 local gridState = {}
-local totalTilesTouched = 0
 
-local function findTile(position)
+--- ( Private Functions ) ---
+local function getRequiredTiles(levelNumber)
+	local levelGridInformation = LevelConfig[tostring(levelNumber)]
+
+	local requiredTiles = 0
+	for colorName, tilePositionNumbers in pairs(levelGridInformation.Colors) do
+		for _, positionNumber in pairs(tilePositionNumbers) do
+			requiredTiles += 1
+		end
+	end
+
+	return requiredTiles
+end
+
+local function checkProgress(levelNumber)
+	local requiredTiles = getRequiredTiles(levelNumber)
+
+	local totalTilesTouched = 0
+
+	for _, gridData in pairs(gridState) do
+		if gridData.level ~= levelNumber then
+			continue
+		end
+
+		totalTilesTouched += 1
+	end
+
+	return totalTilesTouched > requiredTiles
+end
+
+local function validateAnswer(levelNumber)
+	local levelGridInformation = LevelConfig[tostring(levelNumber)]
+
+	for _, gridData in pairs(gridState) do
+		if gridData.level ~= levelNumber then
+			continue
+		end
+
+		local matches = false
+		for colorName, tilePositionNumbers in pairs(levelGridInformation.Colors) do
+			for _, positionNumber in pairs(tilePositionNumbers) do
+				if positionNumber ~= gridData.tileIndex then
+					continue
+				end
+
+				matches = true
+			end
+		end
+
+		if not matches then
+			return false
+		end
+
+		return true
+	end
+end
+
+local function resetGrid(levelNumber)
+	local levelBuild = workspace:FindFirstChild(string.format("Level %s", tostring(levelNumber)))
+	local tileHolder = levelBuild:FindFirstChild("TileHolder")
+
+	local litTiles = CollectionService:GetTagged("NoColor")
+
+	for _, tile in pairs(litTiles) do
+		tile:Destroy()
+	end
+
+	for _, gridData in pairs(gridState) do
+		if gridData.level ~= levelNumber then
+			continue
+		end
+
+		gridData.initialTileObject.Parent = tileHolder
+	end
+end
+
+--- ( Public Functions ) ---
+---@param position table
+function findTile(position)
 	for _, gridData in pairs(gridState) do
 		if gridData.position.X ~= position.X then
 			continue
@@ -32,16 +110,22 @@ local function findTile(position)
 	return nil
 end
 
----@param position Vector3
+---@param position table
 ---@param levelNumber number
 ---@param tileIndex number
-local function handleTile(position, levelNumber, tileIndex)
+function handleTile(position, levelNumber, tileIndex)
 	local levelGridInformation = LevelConfig[tostring(levelNumber)]
 
 	local levelBuild = workspace:FindFirstChild(string.format("Level %s", tostring(levelNumber)))
 
 	local tileHolder = levelBuild:FindFirstChild("TileHolder")
 	assert(tileHolder, string.format("Expected a folder/model named `TileHolder` for level %s", tostring(levelNumber)))
+
+	local playerStart = levelBuild:FindFirstChild("PlayerStart")
+	assert(
+		playerStart,
+		string.format("Expected to find a part named `PlayerStart` for level %s", tostring(levelNumber))
+	)
 
 	-- clone and parent no-color tile and position it based off of the last spawned tiles position.
 	local tileToHandle = tiles.Off:Clone()
@@ -50,6 +134,8 @@ local function handleTile(position, levelNumber, tileIndex)
 
 	local gridData = {
 		level = levelNumber,
+		tileIndex = tileIndex,
+		initialTileObject = tileToHandle,
 		position = {
 			X = tileToHandle.PrimaryPart.Position.X,
 			Y = tileToHandle.PrimaryPart.Position.Y,
@@ -59,8 +145,17 @@ local function handleTile(position, levelNumber, tileIndex)
 	table.insert(gridState, gridData)
 
 	-- Assign tags via CollectionService based on the position of the tile and what level it's being spawned to.
-	for colorName, tilePositionNumber in pairs(levelGridInformation.Colors) do
-		if tilePositionNumber ~= tileIndex then
+	for colorName, tilePositionNumbers in pairs(levelGridInformation.Colors) do
+		local matches = false
+		for _, positionNumber in pairs(tilePositionNumbers) do
+			if positionNumber ~= tileIndex then
+				continue
+			end
+
+			matches = true
+		end
+
+		if not matches then
 			CollectionService:AddTag(tileToHandle, "NoColor")
 		end
 
@@ -74,9 +169,25 @@ local function handleTile(position, levelNumber, tileIndex)
 			return
 		end
 
+		local humanoidRootPart = humanoid.RootPart
+
+		local canSelectTile = checkProgress(levelNumber)
+		if not canSelectTile then
+			local isAnswerValid = validateAnswer(levelNumber)
+
+			if not isAnswerValid then
+				humanoidRootPart.CFrame = CFrame.new(playerStart.Position)
+				resetGrid(levelNumber)
+			else
+				return warn(string.format("Player has cleared Level %s", tostring(levelNumber)))
+			end
+		end
+
 		local newTile = tiles.NoColor:Clone()
 		newTile.Parent = tileHolder
 		newTile:SetPrimaryPartCFrame(tileToHandle.Position)
+
+		CollectionService:AddTag(newTile, "LitTile")
 
 		tileToHandle.Parent = nil
 	end)
