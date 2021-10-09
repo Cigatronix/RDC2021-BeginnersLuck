295dba1620c9d6c0a1e6eb1695deb638a8609426
MacAndSwiss <rzeng3@gmail.com>
2021-10-09 16:40:51 -0700

    Implementing some basic level dialogue as a test for level 1

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -5,6 +5,9 @@ local Dialogue = require(script.modules.dialogue)
 local Defaults = require(script.modules.defaults)
 local ColorControls = require(script.modules.colorcontrols)
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LevelDialogue = require(ReplicatedStorage.shared.strings.LevelDialogue)
+
 -- wait(5)
 
 -- local Test = {"ANGRY", "JOY", "SARCASTIC", "Test"}
---
+++ src/shared/strings/levelDialogue.lua
@@ -0,0 +1,17 @@
+--[[
+    levelDialogue.lua
+
+    Strings mainly. For the companion cube to say.
+]]
+
+local LevelDialogue = {
+    {
+        {"Hey there!", "JOY"},
+        {"I'm a cube. Of glass.", "NEUTRAL"},
+        {"Looks like you're new around here, huh?", "SARCASTIC"},
+        {"Here's the deal - step on these buttons in the right pattern, and you get to move on! Simple as that.", "NEUTRAL"}
+    }
+}
+
+
+return LevelDialogue
