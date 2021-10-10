213878529a225d8d0ec8c53f4e97c22c7b6ddbea
MacAndSwiss <rzeng3@gmail.com>
2021-10-10 08:51:05 -0700

    Added UI changes to indicate Cubug usage

--- src/client/modules/cubug.lua
+++ src/client/modules/cubug.lua
@@ -9,6 +9,7 @@ local Remotes = ReplicatedStorage:WaitForChild("Remotes")
 local ToggleCubug = Remotes:WaitForChild("ToggleCubug")
 local LightSpecificTileColor = Remotes:WaitForChild("LightSpecificTileColor")
 local Controls = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("HUD"):WaitForChild("Controls")
+local ColorControls = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("HUD"):WaitForChild("ColorFrame"):WaitForChild("Cube1")
 
 local Cubug = {}
 
@@ -16,6 +17,19 @@ local function ToggleCubugRemote()
 	ToggleCubug:FireServer()
 	if LocalPlayer:GetAttribute("SELECTED_COLOR") ~= "" then
 		LightSpecificTileColor:FireServer(LocalPlayer:GetAttribute("SELECTED_COLOR"))
+	end
+end
+
+local InCubug = false
+
+local function ToggleCubugUI()
+	InCubug = not InCubug
+	if InCubug then
+		ColorControls.BackgroundColor3 = Color3.fromRGB(255, 208, 0)
+		ColorControls.TextLabel.Text = "Cubug! ^_^"
+	else
+		ColorControls.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		ColorControls.TextLabel.Text = "Cubert"
 	end
 end
 
@@ -28,12 +42,14 @@ Cubug.Initialize = function()
 		if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Q then
 			ToggleCubugRemote()
 			Controls.Cubug.Visible = true
+			ToggleCubugUI()
 		end
 	end)
 
 	Controls.Cubug.TextButton.MouseButton1Click:Connect(function()
 		ToggleCubugRemote()
 		Controls.Cubug.Visible = true
+		ToggleCubugUI()
 	end)
 end
 
