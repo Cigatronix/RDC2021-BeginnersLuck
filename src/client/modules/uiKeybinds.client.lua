7c6b518499ec86730971596d414761e11f1aeb96
MacAndSwiss <rzeng3@gmail.com>
2021-10-09 22:29:00 -0700

    Added button resetting for resetting a room

--- src/client/modules/uiKeybinds.client.lua
+++ src/client/modules/uiKeybinds.client.lua
@@ -1,20 +1,22 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService("UserInputService")
 
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
+
+local Controls = PlayerGui:WaitForChild("HUD"):WaitForChild("Controls")
+
 local resetLevelRemote = ReplicatedStorage:WaitForChild("Remotes").ResetLevel
 
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
 	if gameProcessedEvent then
 		return
 	end
-
-	if input.UserInputType ~= Enum.UserInputType.Keyboard then
-		return
+	if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.R then
+		resetLevelRemote:FireServer()
 	end
+end)
 
-	if input.KeyCode ~= Enum.KeyCode.R then
-		return
-	end
-
+Controls:WaitForChild("Restart").MouseButton1Click:Connect(function()
 	resetLevelRemote:FireServer()
 end)
