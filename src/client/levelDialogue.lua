ab7a8c72aafefcb30be5c503c1b1b65aaa28cf24
Cigatronix <33050240+Cigatronix@users.noreply.github.com>
2021-10-10 03:02:02 -0600

    Update levelDialogue.lua

--- src/client/levelDialogue.lua
+++ src/client/levelDialogue.lua
@@ -6,7 +6,7 @@
 
 local LevelDialogue = {
 	{
-		{ "Hey there!", "JOY", 2, nil, "OpenLobbyElevator" },
+		{ "Hey there!", "JOY", 5, nil, "OpenLobbyElevator" },
 		{ "Looks like you're new around here, huh?", "SARCASTIC", 5 },
 		{
 			"But let's get started anyways! Step into the elevator to begin.",
@@ -17,11 +17,11 @@ local LevelDialogue = {
 		},
 	},
 	{
-		{ "Welcome to the Tile Trials! I hope you're ready to solve some puzzles.", "", 2 },
+		{ "Welcome to the Tile Trials! I hope you're ready to solve some puzzles.", "", 5 },
 		{
 			"Because if you're not...you're gonna have a bad time.",
 			"SARCASTIC",
-			2,
+			5,
 			nil,
 			"OpenLevel1Entrance",
 		},
@@ -30,22 +30,22 @@ local LevelDialogue = {
 		{
 			"Here's the deal - step on these buttons in the right pattern, and you get to move on! Simple as that.",
 			"NEUTRAL",
-			2,
+			5,
 			true,
 		},
 	},
 	{
-		{ "Hopefully this works this time... Try to step on the colored tiles again.", "NEUTRAL", 2, true },
+		{ "Hopefully this works this time... Try to step on the colored tiles again.", "NEUTRAL", 5, true },
 	},
 	{
-		{ "Nice job, you did it! But it won't be this simple next time.", "JOY", 2 },
+		{ "Nice job, you did it! But it won't be this simple next time.", "JOY", 5 },
 	},
 	{
-		{ "For this one, you'll have to put me at the station to check which tiles are correct.", "NEUTRAL", 2, true },
-		{ "..Don't put me down anywhere else. Please.", "ANGRY", 2 },
+		{ "For this one, you'll have to put me at the station to check which tiles are correct.", "NEUTRAL", 5, true },
+		{ "..Don't put me down anywhere else. Please.", "ANGRY", 5 },
 	},
 	{
-		{ "Nice one. Hopefully the next one won't take you too long.", "NEUTRAL", 2 },
+		{ "Nice one. Hopefully the next one won't take you too long.", "NEUTRAL", 5 },
 	},
 }
 
