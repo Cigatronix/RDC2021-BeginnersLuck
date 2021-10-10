8c0716997b872fc7aa0c1a5cc70426df09505841
MacAndSwiss <rzeng3@gmail.com>
2021-10-09 23:17:04 -0700

    Maybe fix for dialogue

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -6,9 +6,12 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LevelDialogue = require(ReplicatedStorage.Shared.strings.levelDialogue)
 
 --TODO: Remove in production, this is purely for testing the dialogue system with real data.
-for _, DialogueTree in ipairs(LevelDialogue) do
-    for _, DialoguePiece in ipairs(DialogueTree) do
-        Dialogue.Speak(DialoguePiece[1], DialoguePiece[2], DialoguePiece[3], DialoguePiece[4])
-    end
-end
+-- for _, DialogueTree in ipairs(LevelDialogue) do
+--     for _, DialoguePiece in ipairs(DialogueTree) do
+--         Dialogue.Speak(DialoguePiece[1], DialoguePiece[2], DialoguePiece[3], DialoguePiece[4])
+--     end
+-- end
 --
+local StartLevel = LevelDialogue[3]
+
+Dialogue.Speak(StartLevel[1], StartLevel[2], StartLevel[3], StartLevel[4])
