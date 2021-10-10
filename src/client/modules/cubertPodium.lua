2f58b82fa8cedc32f8eb066bd56105919e6136f4
MacAndSwiss <rzeng3@gmail.com>
2021-10-10 00:24:52 -0700

    Added additional functionality to the prompts

--- src/client/modules/cubertPodium.lua
+++ src/client/modules/cubertPodium.lua
@@ -1,6 +1,12 @@
 local CollectionService = game:GetService("CollectionService")
 local WorkspaceService = game:GetService("Workspace")
 local Cubert = WorkspaceService:WaitForChild("Cubert")
+local TweenService = game:GetService("TweenService")
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+
+local GenericTweenInformation = TweenInfo.new(0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
 
 local CubertWeld
 
@@ -11,6 +17,16 @@ local function UnweldCubert()
         CubertWeld:Destroy()
         CubertWeld = nil
     end
+    local Humanoid = Character:FindFirstChild("Humanoid")
+    if Humanoid and Humanoid.WalkSpeed == 0 then
+        Humanoid.WalkSpeed = 16
+    end
+
+    local Camera = WorkspaceService.CurrentCamera
+    local TweenToPlayer = TweenService:Create(Camera, GenericTweenInformation, {Position = Character:GetPivot().Position + (Character:GetPivot().UpVector * 2)})
+    TweenToPlayer:Play()
+    TweenToPlayer.Completed:Wait()
+    Camera.CameraType = Enum.CameraType.Custom
 end
 
 local function WeldCubert(PromptParent)
@@ -22,6 +38,30 @@ local function WeldCubert(PromptParent)
     CubertWeld.Part0 = Cubert.PrimaryPart
     CubertWeld.Part1 = PromptParent
     CubertWeld.Parent = Cubert.PrimaryPart
+
+    --[[
+        TODO:
+            - Anchor player -> WalkSpeed 0 //
+            - Fade to black (or maybe tween camera? /shrug)
+            - UI on
+            - View on
+            - Camera set to base
+
+
+            - Invisible player's cube //
+            - Visible podium cube //
+            --> Just weld it lmao
+    ]]
+    local Humanoid = Character:FindFirstChild("Humanoid")
+    if Humanoid and Humanoid.WalkSpeed ~= 0 then
+        Humanoid.WalkSpeed = 0
+    end
+
+    local Camera = WorkspaceService.CurrentCamera
+    Camera.CameraType = Enum.CameraType.Scriptable
+    local TweenToCubert = TweenService:Create(Camera, GenericTweenInformation, {Position = Cubert.Position})
+    TweenToCubert:Play()
+    --TODO: Check if the thing is going to be facing forward, also how should we handle rotation?
 end
 
 local function ToggleCubertWeld(Prompt, PromptParent)
